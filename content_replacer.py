994bb93b989426201865b1e68a5c8101f7bf8995
Lashmanbala <lashmanbala@gmail.com>
2025-10-31 11:05:04 +0530

    Updating the new content in the existing page

--- content_replacer.py
+++ content_replacer.py
@@ -10,7 +10,7 @@ wp_username = os.getenv("WP_USERNAME")
 wp_app_pasword = os.getenv("WP_APP_PASSWORD")
 WP_BASE = os.getenv("WP_URL")
 
-page_url = 'https://www.loclite.co.uk/top-rated-electricians-in-aberdeen/'
+page_url = 'https://www.loclite.co.uk/test_page/'
 
 slug = urlparse(page_url).path.strip("/")
 
@@ -30,3 +30,21 @@ page_id = data[0]["id"]
 
 print(page_id)
 print(data)
+
+new_content = "<p>new data new data new data</p>"
+
+payload = {
+    "content": new_content
+}
+
+update_res = requests.post(
+    f"{WP_BASE}/{page_id}",
+    auth=HTTPBasicAuth(wp_username, wp_app_pasword),
+    json=payload
+)
+
+if update_res.status_code == 200:
+    print(f"✅ Content updated for page: {page_url}")
+else:
+    print(f"❌ Failed to update page content: {update_res.status_code}")
+    print(update_res.text)
